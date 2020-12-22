3862aea1351e445878c02598350dd180cc70b865
unknown <shanakaromesh@ymail.com>
2020-12-22 13:28:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_app/screens/home/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Furniture App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        textTheme:
+            GoogleFonts.dmSansTextTheme().apply(displayColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
