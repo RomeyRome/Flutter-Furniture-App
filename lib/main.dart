b22299f4241a99b53a0a5cf43ba5df1a86b31e1f
unknown <shanakaromesh@ymail.com>
2020-12-22 14:55:23 +0530

    complete the data fetching and the UI screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:furniture_app/constants.dart';
 import 'package:furniture_app/screens/home/home_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,8 +16,16 @@ class MyApp extends StatelessWidget {
       title: 'Furniture App',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
+        // Here we set DM Sans as our default fonts
+        // Now we also apply out text color to all flutter textTheme
         textTheme:
             GoogleFonts.dmSansTextTheme().apply(displayColor: kTextColor),
+        // Almost all of our app bar have this style
+        appBarTheme: AppBarTheme(
+          color: Colors.transparent,
+          elevation: 0,
+          brightness: Brightness.light,
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomeScreen(),
